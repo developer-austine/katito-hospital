777b998833492d4537b94e13f09d998d245ba162
Elvis Kemoi <elviskemoi001@gmail.com>
2024-05-20 00:17:20 +0300

    Almost Finished. Setting up the smtp is what is left.

---
+++ js/animation.js
@@ -0,0 +1,94 @@
+// Initialize ScrollReveal
+ScrollReveal().reveal(".logo", {
+	distance: "500px", // Distance to move (e.g., 50px)
+	origin: "top", // Animation starts from the top
+	duration: 1000, // Animation duration in milliseconds
+	easing: "ease-in-out", // Easing function for the animation
+	delay: 200, // Delay before the animation starts
+	//  // Whether the animation should reset when element leaves viewport
+});
+ScrollReveal().reveal("#home .container", {
+	distance: "500px",
+	origin: "left",
+	duration: 1000,
+	easing: "ease-in-out",
+	delay: 200,
+	//
+});
+
+ScrollReveal().reveal("#vision", {
+	distance: "500px",
+	origin: "right",
+	duration: 1000,
+	easing: "ease-in-out",
+});
+
+ScrollReveal().reveal("#mission", {
+	distance: "500px",
+	origin: "right",
+	duration: 1000,
+	easing: "ease-in-out",
+});
+ScrollReveal().reveal("#value", {
+	distance: "500px",
+	origin: "right",
+	duration: 1000,
+	easing: "ease-in-out", // Easing function for the animation
+});
+
+ScrollReveal().reveal(".about", {
+	distance: "500px",
+	origin: "bottom",
+	duration: 1000,
+	delay: "300",
+	reset: true,
+});
+
+ScrollReveal().reveal(".icon-box", {
+	distance: "500px",
+	origin: "bottom",
+	duration: 1000,
+	delay: "300",
+});
+ScrollReveal().reveal("#docimage", {
+	distance: "500px",
+	origin: "left",
+	duration: 1000,
+	delay: "300",
+});
+ScrollReveal().reveal(".appointment-btn", {
+	distance: "500px",
+	origin: "right",
+	duration: 1000,
+});
+ScrollReveal().reveal(".services .col", {
+	distance: "500px",
+	origin: "bottom",
+	duration: 1000,
+	delay: "300",
+});
+ScrollReveal().reveal(".appointment", {
+	distance: "500px",
+	origin: "bottom",
+	duration: 1000,
+	delay: "1000",
+});
+
+ScrollReveal().reveal(".doctors .col-lg-6", {
+	distance: "500px",
+	origin: "bottom",
+	duration: 1000,
+	delay: "300",
+});
+// ScrollReveal().reveal("#ourNews", {
+// 	distance: "500px",
+// 	origin: "bottom",
+// 	duration: 1000,
+// 	delay: "300",
+// });
+ScrollReveal().reveal("#ourNews .row", {
+	distance: "500px",
+	origin: "bottom",
+	duration: 1000,
+	delay: "300",
+});
